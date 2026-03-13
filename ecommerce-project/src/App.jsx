9ab0388f71bd0d638b6c7fbea4404615ba689c29
Bhone Thank Kyaw <bhonethankmac6@.gmail.com>
2026-03-13 23:18:44 +0630

    add all pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePages } from "./pages/HomePages";
+import { CheckOutPage } from "./pages/CheckOutPage";
+import { Orders } from "./pages/OrdersPage";
 
 function App() {
   return (
     <>
-      <HomePages />
+      <Routes>
+        <Route path="/" element={<HomePages />}></Route>
+        <Route path="/checkout" element={<CheckOutPage />}></Route>
+        <Route path="/order" element={<Orders />}></Route>
+      </Routes>
     </>
   );
 }
